2fd7b1f8546adb23758e3e43858a927e3ec6baaf
Andrew <32679171+andrewtrots@users.noreply.github.com>
2022-05-05 23:01:57 +0300

    Sample Code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-alert('Hello JavaScript!');
+// alert('Hello!');
+
+let greetings = "World!";
+// alert(greetings);
+
+const message = "Hello " + greetings;
+alert(message);
+const userAge = parseFloat(prompt("Enter your age"));
+alert("Your age is: " + userAge);
+
+const nextAge = add(userAge, 1);
+alert(nextAge);
+// > < >= <= === == !== != && ||
+if (userAge >= 18) {
+  alert("You are adult");
+} else {
+  alert("You are underaged");
+}
+
+function add(a, b) {
+  const result = a + b;
+  return result;
+}
